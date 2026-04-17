4671b0ed2981750f479ac164bf673d531a34590a
sandy10 <025945@aSd>
2026-04-17 18:59:36 +0530

    error handling in ViewModel

--- app/src/main/java/com/sandeep/countincrementthroughc/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sandeep/countincrementthroughc/viewmodel/MainViewModel.kt
@@ -1,29 +1,51 @@
 package com.sandeep.countincrementthroughc.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.sandeep.countincrementthroughc.model.Counter
-import com.sandeep.countincrementthroughc.nativeLib.NativeBridge
+import androidx.lifecycle.viewModelScope
+import com.sandeep.countincrementthroughc.data.model.Counter
+import com.sandeep.countincrementthroughc.data.repository.CounterRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val repository: CounterRepository = CounterRepository()
+) : ViewModel() {
 
     private val _data = MutableStateFlow<List<Counter>>(emptyList())
     val items = _data.asStateFlow()
 
     init {
-        NativeBridge.onResult = { count ->
-            val timeStamp = SimpleDateFormat("HH:mm:ss a", Locale.getDefault()).format(Date())
-            val newItem = Counter(count, timeStamp)
+        observeCounter()
+    }
 
-            _data.value += newItem
+    private fun observeCounter() {
+        viewModelScope.launch {
+            repository.counterFlow.collect { count ->
+
+                val timeStamp = SimpleDateFormat("HH:mm:ss a", Locale.getDefault()).format(Date())
+
+                val newItem = Counter(count, timeStamp)
+                _data.value += newItem
+            }
         }
     }
 
     fun onButtonClick() {
-        NativeBridge.incrementCount()
+        viewModelScope.launch {
+            withTimeoutOrNull(3000) {
+                repository.increment()
+            } ?: Log.e("VM", "Timeout from native")
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        repository.cleanup()
     }
 }
